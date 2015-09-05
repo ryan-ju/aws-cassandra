0149497337916ae64ce32172094b25031bc9607c
ryan-ju <r.ju2014@gmail.com>
2015-09-05 18:24:49 +0100

    Upgraded Git and removed deleted files

--- install_chef.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Install Chef
-echo "Installing Chef ..."
-sudo curl -L https://www.opscode.com/chef/install.sh | bash >> /var/log/vagrant/install_chef.log
-echo "Installed Chef!"
--- load-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Please enter node IPs in the form '192.168.2.1,192.168.2,192.168.3':"
-read NODES
-cassandra-stress user profile=./load-test.yaml ops\(singlepost=10,timeline=5,insert=1\) n=1000 truncate=always -node "$NODES"
